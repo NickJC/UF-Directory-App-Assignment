16b610f01b58357537ce3437286fc23baf27fae5
Nicholas Cummings <nickjcu@ufl.edu>
2016-01-18 21:20:42 -0500

    completed assignment#2

--- listingController.js
+++ listingController.js
@@ -2,13 +2,51 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
-
+    $scope.sortType = "code";
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      var newItem = createItem();
+        newItem.code = $scope.itemCode;
+        newItem.name = $scope.itemName;
+        newItem.coordinates.latitude=$scope.itemLat;
+        newItem.coordinates.longitude=$scope.itemLong;
+        newItem.address=$scope.itemAdd;
+        $scope.listings.push(newItem);
+        $scope.itemCode = "";
+        $scope.itemName = "";
+        $scope.itemLat="";
+        $scope.itemLong="";
+        $scope.itemAdd="";
+    };
+      
+    $scope.deleteListing = function(index) {
+        $scope.listings.splice(index, 1);
+        $scope.detailedInfo = undefined;
+    };
+      
+
+  $scope.showDetails = function(index) {
+    $scope.detailedInfo = $scope.listings[index];
+    var latLng = {
+        lat: Number($scope.detailedInfo.coordinates.latitude), 
+        lng: Number($scope.detailedInfo.coordinates.longitude)
+      }; 
+    };
   }
 ]);
+
+function createItem(){
+    var item = {
+        code : '', 
+        name:  '',
+        coordinates: {
+                latitude: 0, 
+                longitude: 0
+            }, 
+        address: ''
+    };
+    return item;
+};
