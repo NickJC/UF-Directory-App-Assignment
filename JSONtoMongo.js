50cb8a978fe7aad414cb3250d3500aae7894b2a7
Nicholas Cummings <nickjcu@ufl.edu>
2016-01-20 21:02:44 -0500

    code is now functional

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -8,7 +8,68 @@ var fs = require('fs'),
     Schema = mongoose.Schema, 
     Listing = require('./ListingSchema.js'), 
     config = require('./config');
+    
+var listingData=JSON.parse(fs.readFileSync('listings.json', 'utf8'));
+// Connect to the db
+//mongoose.connect(config.db.uri);
 
+
+//mongoose.connect(config.db.uri);
+
+//fs.readFile('listings.json', 'utf8', function(err, data) {
+//  listingData=data;//data is stored in memory
+  
+  //var length = listingData.length;
+  //server is created
+//});
+
+
+mongoose.connect(config.db.uri, function(err, db) {
+  if (err) throw err;
+  //console.log("Connected to Database");
+  /*var doc = {
+        code : '',
+        name:  '',
+        coordinates: {
+                latitude: 0, 
+                longitude: 0
+            }, 
+        address: ''
+        //created_at: Date,
+        //updated_at: Date,
+};*/
+  //simple json record
+  console.log(listingData.entries[0].code);
+  for (var i=0;i<listingData.entries.length;i++)
+  {
+listingData.entries[i].coordinates=listingData.entries[i].coordinates||{};
+listingData.entries[i].address=listingData.entries[i].address||"";
+    var doc = Listing({
+      code: listingData.entries[i].code,
+      name:  listingData.entries[i].name,
+        coordinates: {
+                latitude: listingData.entries[i].coordinates.latitude, 
+                longitude: listingData.entries[i].coordinates.longitude
+            }, 
+        address: listingData.entries[i].address
+    });
+//doc.code=listingData.entries[i].code;
+//doc.name=listingData.entries[i].name;
+//doc.coordinates.latitude=listingData.entries[i].coordinates.latitude;
+//doc.coordinates.longitude=listingData.entries[i].coordinates.longitude;
+//doc.address=listingData.entries[i].address;
+
+console.log(doc.code);
+//insert record
+  doc.save(doc, function(err,record) {
+    if (err) throw err;
+    console.log("Record added as ");
+  });
+
+  }
+  
+  
+  });
 /* Connect to your database */
 
 /* 
--- queries.js
+++ queries.js
@@ -1,31 +1,83 @@
-/* Fill out these functions using Mongoose queries*/
+// Fill out these functions using Mongoose queries
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema; 
+ var  Listing = require('./ListingSchema.js'); 
+var config = require('./config');
 
+
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+/*
 var findLibraryWest = function() {
-  /* 
-    Find the document that contains data corresponding to Library West,
-    then log it to the console. 
-   */
+  
+    //Find the document that contains data corresponding to Library West,
+    //then log it to the console. 
+   console.log('Hey dude');
+mongoose.connect(config.db.uri, function(err, listing) {
+  if (err) throw err;
+
+   Listing.find({code: 'LBW'},function(listing){
+    if (err) throw err;
+
+    console.log(listing);
+ });
+
+
+
+});
+
+
 };
+*/
+/*
 var removeCable = function() {
   /*
     Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
-   */
+   
+   Listing.find({code: 'CABL'},function(err,listing){
+listing.remove(function(listing){console.log('deleted');
+      });
+   });
 };
 var updatePhelpsMemorial = function() {
   /*
     Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
-   */
+   
+   Listing.find({code:'PHL'},{address: 'Gainesville,FL'},function(err,listing){
+    console.log(listing);
+   });
 };
-var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
-   */
+*/
+var retrieveAllListings = function(db, callback) {
+  
+  //  Retrieve all listings in the database, and log them to the console. 
+   
+   //Listing.find({},function(err,listings){
+    //if(err) throw err;
+    //console.log(listings);
+   //});
+  var cursor =db.collection('listings').find( );
+   cursor.each(function(err, doc) {
+      assert.equal(err, null);
+      if (doc != null) {
+         console.log(doc);
+      } else {
+         callback();
+      }
+   });
 };
 
-findLibraryWest();
-removeCable();
-updatePhelpsMemorial();
-retrieveAllListings();
+//findLibraryWest();
+console.log('done');
+//removeCable();
+//updatePhelpsMemorial();
+MongoClient.connect(config.db.uri, function(err, db) {
+  assert.equal(null, err);
+retrieveAllListings(db,function(){
+  db.close;
+});
+});
